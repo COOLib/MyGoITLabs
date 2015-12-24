b8f4d72fc67cb41d5fe20fa11c2bad45ec02e0f4
COOLib <COOLib@yandex.ru>
2015-12-24 22:27:37 +0200

    Module 5

--- Module5/MaxAndMin.java
+++ Module5/MaxAndMin.java
@@ -5,20 +5,38 @@ public class MaxAndMin {
 
     public int getMaxElement(int[] array) {
         int max = 0;
-        for (int i = 0; i < array.length; i++) {
-            if (array[0] < array[i]) {
-                array[0] = array[i];
-            }
-        }
+       while (true) {
+           if (array == null) {
+               System.out.println("Please enter not null array.");
+           } else if (array.length == 0) {
+               System.out.println("Please enter array with not null length.");
+           } else {
+               for (int i = 0; i < array.length; i++) {
+                   if (array[0] < array[i]) {
+                       array[0] = array[i];
+                   }
+               }
+               break;
+           }
+       }
         max = array[0];
         return max;
     }
 
     public int getMinElement(int[] array) {
         int min = 0;
-        for (int i = 0; i < array.length; i++) {
-            if (array[0] > array[i]) {
-                array[0] = array[i];
+        while (true) {
+            if (array == null) {
+                System.out.println("Please enter not null array.");
+            } else if (array.length == 0) {
+                System.out.println("Please enter array with not null length.");
+            } else {
+                for (int i = 0; i < array.length; i++) {
+                    if (array[0] > array[i]) {
+                        array[0] = array[i];
+                    }
+                }
+                break;
             }
         }
         min = array[0];
