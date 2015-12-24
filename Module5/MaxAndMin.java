ecf6377edcb396c30a776cf790c373b2a4bbf4ed
COOLib <COOLib@yandex.ru>
2015-12-24 22:58:03 +0200

    Module 5

--- Module5/MaxAndMin.java
+++ Module5/MaxAndMin.java
@@ -4,42 +4,35 @@
 public class MaxAndMin {
 
     public int getMaxElement(int[] array) {
-        int max = 0;
-       while (true) {
-           if (array == null) {
-               System.out.println("Please enter not null array.");
-           } else if (array.length == 0) {
-               System.out.println("Please enter array with not null length.");
-           } else {
-               for (int i = 0; i < array.length; i++) {
-                   if (array[0] < array[i]) {
-                       array[0] = array[i];
-                   }
-               }
-               break;
-           }
-       }
-        max = array[0];
+        int  max = array[0];
+        if (array == null) {
+            System.out.println("Please, enter not null array.");
+        } else if (array.length == 0) {
+            System.out.println("Please, enter array with not null length.");
+        } else {
+            for (int i = 0; i < array.length; i++) {
+                if (max < array[i]) {
+                    max = array[i];
+                }
+            }
+        }
+
         return max;
     }
 
     public int getMinElement(int[] array) {
-        int min = 0;
-        while (true) {
-            if (array == null) {
-                System.out.println("Please enter not null array.");
-            } else if (array.length == 0) {
-                System.out.println("Please enter array with not null length.");
-            } else {
-                for (int i = 0; i < array.length; i++) {
-                    if (array[0] > array[i]) {
-                        array[0] = array[i];
-                    }
+        int min = array[0];
+        if (array == null) {
+            System.out.println("Please, enter not null array.");
+        } else if (array.length == 0) {
+            System.out.println("Please, enter array with not null length.");
+        } else {
+            for (int i = 0; i < array.length; i++) {
+                if (min > array[i]) {
+                    min = array[i];
                 }
-                break;
             }
         }
-        min = array[0];
         return min;
     }
 }
