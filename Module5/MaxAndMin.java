cc98ad8dc453a181262a3b246cc42700e33685b8
COOLib <COOLib@yandex.ru>
2015-12-25 23:02:21 +0200

    Module 5

--- Module5/MaxAndMin.java
+++ Module5/MaxAndMin.java
@@ -1,19 +1,16 @@
-/**
  * Created by COOLib on 24.12.2015.
  */
 public class MaxAndMin {
 
-    public int getMaxElement(int[] array) {
+    public static int getMaxElement(int[] array) {
 
         if (array == null) {
-
             return Integer.MAX_VALUE;
         } else if (array.length == 0) {
-
             return Integer.MAX_VALUE;
         } else {
             int max = array[0];
-            for (int i = 0; i < array.length; i++) {
+            for (int i = 1; i < array.length; i++) {
                 if (max < array[i]) {
                     max = array[i];
                 }
@@ -22,14 +19,15 @@ public class MaxAndMin {
         }
     }
 
-    public int getMinElement(int[] array) {
-        int min = 0;
+    public static int getMinElement(int[] array) {
+
         if (array == null) {
             return Integer.MIN_VALUE;
         } else if (array.length == 0) {
             return Integer.MIN_VALUE;
         } else {
-            for (int i = 0; i < array.length; i++) {
+            int min = array[0];
+            for (int i = 1; i < array.length; i++) {
                 if (min > array[i]) {
                     min = array[i];
                 }
