489d323f9b0abf6e6ab1e7aa8811c005adfdc97a
COOLib <COOLib@yandex.ru>
2016-01-15 17:26:43 +0200

    Files before correction

---
+++ Module7/Tasks/Module3.Files/Before/File.java
@@ -0,0 +1,25 @@
+﻿import java.util.Date;
+
+public abstract class File {
+
+     /*Here, instead of creating a String and output to the console in each subclass,
+      You can call it from a superclass
+     */
+
+    private String name;
+    private int size;
+    private Date date;
+
+    public File(String name, int size, Date date) {
+        this.name = name;
+        this.size = size;
+        this.date = date;
+    }
+
+    public String getName() {return name;}
+    public int getSize() {return size;}
+    public Date getDate() {return date;}
+
+    public abstract void open();
+    public abstract void close();
+}
