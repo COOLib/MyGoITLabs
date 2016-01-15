f809f37729e720783ca08a8b4110f453650f56f0
COOLib <COOLib@yandex.ru>
2016-01-15 16:49:06 +0200

    Anti-pattern correction in module 4

---
+++ Module7/Tasks/Module4/Temperature.java
@@ -0,0 +1,15 @@
+/**
+ * Created by COOLib on 21.12.2015.
+ */
+public class Temperature {
+
+	//Здесь есть анти-паттерн Magic Number(Example: 1.8, 32)
+
+    public double toFahrenheit(int degreeInCelsius) {
+        return 1.8 * degreeInCelsius + 32;
+    }
+
+    public double toCelsius(int degreeInFahrenheit) {
+        return (degreeInFahrenheit - 32) / 1.8;
+    }
+}
