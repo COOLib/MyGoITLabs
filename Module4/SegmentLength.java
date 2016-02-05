9c0e7d1deca2e0e34749e6c05f6a10e694599ca1
COOLib <COOLib@yandex.ru>
2016-02-05 23:52:51 +0200

    Delete SegmentLength.java

--- Module4/SegmentLength.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by COOLib on 21.12.2015.
- */
-public class SegmentLength {
-
-    public double calculateSegmentLength(int xA, int xB, int yA, int yB) {
-        return Math.sqrt(Math.pow((xB - xA), 2) + Math.pow((yB - yA), 2));
-    }
-}
