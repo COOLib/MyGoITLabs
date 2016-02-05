6d9e6f34f22af64e43cebfe0f06c92c38f0cc76a
COOLib <COOLib@yandex.ru>
2016-02-05 23:33:24 +0200

    Array sorting and look for max/min

---
+++ Module5/src/MaxAndMin.java
@@ -0,0 +1,36 @@
+public class MaxAndMin {
+
+    public static int getMaxElement(int[] array) {
+
+        if (array == null) {
+            return Integer.MAX_VALUE;
+        } else if (array.length == 0) {
+            return Integer.MAX_VALUE;
+        } else {
+            int max = array[0];
+            for (int i = 1; i < array.length; i++) {
+                if (max < array[i]) {
+                    max = array[i];
+                }
+            }
+            return max;
+        }
+    }
+
+    public static int getMinElement(int[] array) {
+
+        if (array == null) {
+            return Integer.MIN_VALUE;
+        } else if (array.length == 0) {
+            return Integer.MIN_VALUE;
+        } else {
+            int min = array[0];
+            for (int i = 1; i < array.length; i++) {
+                if (min > array[i]) {
+                    min = array[i];
+                }
+            }
+            return min;
+        }
+    }
+}
